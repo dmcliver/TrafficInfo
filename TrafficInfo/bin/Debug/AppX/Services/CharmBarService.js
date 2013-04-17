1585dbcb20fdf7aeae45f3a2fd67692b3e819947
dmcliver <dmcliver@gmail.com>
2013-04-17 13:52:41 +1200

    Cleaned up code

---
+++ TrafficInfo/bin/Debug/AppX/Services/CharmBarService.js
@@ -0,0 +1,38 @@
+﻿var CharmBarService = (function() {
+
+    "use strict";
+
+    var self = this;
+    this.getCurrentSelectedCamera = null;
+    
+    var dtm = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
+    
+    dtm.addEventListener("datarequested", function (e) {
+
+        if (self.getCurrentSelectedCamera != null && self.getCurrentSelectedCamera() != null) {
+
+            var deferral = e.request.getDeferral();
+            setShareDetails(e);
+            var uri = setUri(e);
+            setUriStream(e, uri);
+            deferral.complete();
+        }
+    });
+    
+    function setShareDetails(e) {
+        e.request.data.properties.title = "Traffic camera image";
+        e.request.data.properties.description = "Traffic situation from selected camera";
+    }
+    
+    function setUri(e) {
+        var uri = new Windows.Foundation.Uri(self.getCurrentSelectedCamera());
+        e.request.data.setUri(uri);
+        return uri;
+    }
+    
+    function setUriStream(e, uri) {
+        var uriStream = Windows.Storage.Streams.RandomAccessStreamReference.createFromUri(uri);
+        e.request.data.properties.thumbnail = uriStream;
+    }
+});
+
