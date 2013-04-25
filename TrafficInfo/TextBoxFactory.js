138f5d0dc54dc8d2d43f12224b960d658d110afd
dmcliver <dmcliver@gmail.com>
2013-04-26 10:29:17 +1200

    Added textbox factory

---
+++ TrafficInfo/TextBoxFactory.js
@@ -0,0 +1,12 @@
+﻿var TextBoxFactory = function() {
+
+
+    this.createTextBox = function(txtBxId, list) {
+
+        var txtBox = new AutoCompleteTextBox(txtBxId, list);
+        var locAutoCompleteStrategy = new AutoCompleteStrategy(new TextBoxAutoCompleteBehaviour(txtBox));
+        locAutoCompleteStrategy.wireDataSourceToTxtBox();
+        return txtBox;
+    };
+};
+
